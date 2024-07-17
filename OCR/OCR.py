54fadba079fefc7ec8b180870d950abf1b549e7c
Anawat <anawatbumroongsri084@gmail.com>
2024-07-17 20:42:24 +0700

    Done concatenate two boxs

--- OCR/OCR.py
+++ OCR/OCR.py
@@ -6,6 +6,7 @@ import cv2
 class EasyOCR():
     def __init__(self):
         self.reader = easyocr.Reader(["th"])
+
     def getCenter(self, corners):
         x_coords = [corner[0] for corner in corners]
         y_coords = [corner[1] for corner in corners]
@@ -14,8 +15,8 @@ class EasyOCR():
         center_y = np.mean(y_coords)
 
         return (center_x, center_y)
+    
     def isSameBox(self, box1, box2, imageHeight, imageWidth): # box1 is left one while box2 is right one
-        print(box1)
         center1 = self.getCenter(box1)
         center2 = self.getCenter(box2)
         if(center1[0] > center2[0]):
@@ -29,6 +30,26 @@ class EasyOCR():
         if((minXRight - maxXLeft) > constants.XBBOX_DIS_THRESHOLD * imageWidth):
             return False
         return True
+    
+    def concateBoxes(self, box1, box2):
+        x_coords = [corner[0] for corner in box1[0]] + [corner[0] for corner in box2[0]]
+        y_coords = [corner[1] for corner in box1[0]] + [corner[1] for corner in box2[0]]
+
+        # Find the minimum and maximum x and y coordinates
+        min_x = min(x_coords)
+        max_x = max(x_coords)
+        min_y = min(y_coords)
+        max_y = max(y_coords)
+
+        # Define the corners of the new bounding box
+        new_bbox = [[min_x, min_y], [max_x, min_y], [max_x, max_y], [min_x, max_y]]
+
+        center1 = self.getCenter(box1[0])
+        center2 = self.getCenter(box2[0])
+        if(center1[0] > center2[0]):
+            box1, box2 = box2, box1
+        
+        return (new_bbox, box1[1]+" "+box2[1], (box1[2]+box2[2])/2)
 
     def readText(self, path):
         results = self.reader.readtext(path)
@@ -36,7 +57,7 @@ class EasyOCR():
         image = cv2.imread(path)
         imageHeight, imageWidth = image.shape[:2]
 
-        print(f"Height : {imageHeight}, Width : {imageWidth}")
+        # print(f"Height : {imageHeight}, Width : {imageWidth}")
 
         ret = []
         for result in results:
@@ -44,10 +65,20 @@ class EasyOCR():
                 ret.append(result)
         
         for i in range(len(ret)):
+            if(ret[i] is None):
+                continue
             for j in range(i):
+                if(ret[j] is None):
+                    continue
                 if(self.isSameBox(ret[i][0],ret[j][0],imageHeight,imageWidth)):
                     # Join box i and box j
-                    print(f"Join box {ret[i][0]} and box {ret[j][0]}")
-                    pass
+                    newBox = self.concateBoxes(ret[i], ret[j])
+                    # print(f"Join box {ret[i][0]} and box {ret[j][0]} into {newBox[0]}")
+                    # Update the bounding box in the list
+                    ret[j] = newBox
+                    ret[i] = None
+                    break
+            
+        ret = [box for box in ret if box is not None]
 
         return ret
--- constants.py
+++ constants.py
@@ -3,3 +3,7 @@ IMG_SIZE_THRESHOLD = 3500 # This can be pixel^2 because it also shows image reso
 OCR_CONF_THRESHOLD = 0.2
 YBBOX_DIS_THRESHOLD = 0.1 # This one and below should be fraction because not all images have same resolution
 XBBOX_DIS_THRESHOLD = 0.12
+
+# Positions
+PLATE_NUM_XPOS = 0.5
+PLATE_NUM_YPOS = 0.33
