104c77a867ee25131d0cea6185e44d87beb15a3c
Anawat <anawatbumroongsri084@gmail.com>
2024-07-18 15:22:18 +0700

    Draw rectangle in image

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 # Thresholds
 IMG_SIZE_THRESHOLD = 3500 # This can be pixel^2 because it also shows image resolution
 OCR_CONF_THRESHOLD = 0.2
-YBBOX_DIS_THRESHOLD = 0.1 # This one and below should be fraction because not all images have same resolution
+YBBOX_DIS_THRESHOLD = 0.15 # This one and below should be fraction because not all images have same resolution
 XBBOX_DIS_THRESHOLD = 0.12
 
 # Positions
--- main.py
+++ main.py
@@ -1,19 +1,44 @@
+import numpy as np
 import cv2
+from PIL import Image, ImageDraw, ImageFont
+
 from Plate_Detector import PlateDetector
 from OCR import EasyOCR
 import constants
 
+def readEasyOCR(path, img_height, img_width):
+    reader = EasyOCR()
+    ocr_results = reader.readText(path)
+
+    plate_number = "None"
+    min_dist = -1
+    for ocr_result in ocr_results:
+        center = reader.getCenter(ocr_result[0])
+        cur_dist = abs(center[0] - img_width * constants.PLATE_NUM_XPOS) + abs(center[1] - img_height * constants.PLATE_NUM_YPOS)
+        if(min_dist == -1):
+            min_dist = cur_dist
+            plate_number = ocr_result[1]
+        if(cur_dist < min_dist):
+            min_dist = cur_dist
+            plate_number = ocr_result[1]
+
+    return plate_number
+
 def main():
-    picture_number = 5
+    picture_number = 1
 
     path_to_img = f"Thai_Plate/{picture_number}.jpg"
 
     plateDetector = PlateDetector()
     bboxes = plateDetector.getBBox(path_to_img)
-    
-    reader = EasyOCR()
 
     img_cv = cv2.imread(path_to_img)
+
+    img_pil = Image.fromarray(cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(img_pil)
+
+    thai_font_path = 'Fonts/NotoSansThaiLooped-Regular.ttf'  # Example path to a Thai font
+    font = ImageFont.truetype(thai_font_path, size=20)
 
     for i, (x1, y1, x2, y2) in enumerate(bboxes): # (x_min, y_min, x_max, y_max) format    
         x1 = int(x1)
@@ -22,18 +47,28 @@ def main():
         y2 = int(y2)
 
         img_size = (y2-y1)*(x2-x1)
+        img_height, img_width = y2-y1, x2-x1
+
+        draw.rectangle([(x1, y1), (x2, y2)], outline='green', width=2)
 
         if(img_size < constants.IMG_SIZE_THRESHOLD): # Image size threshold
-            print(f"Image {i+1} is too small")
+            draw.text((x1, y1 - 30), "Plate is too small", font=font, fill='green')
             continue
 
         cropped_img = img_cv[y1:y2, x1:x2]
 
         preprocessed_img = plateDetector.preprocessImg(cropped_img)
-
         cv2.imwrite(f"Cropped_{i+1}.jpg", preprocessed_img)
 
-        print(reader.readText(f"Cropped_{i+1}.jpg"))
+        plate_number_EasyOCR = readEasyOCR(f"Cropped_{i+1}.jpg", img_height, img_width)
+
+        draw.text((x1, y1 - 30), f"EasyOCR : {plate_number_EasyOCR}", font=font, fill='green')
+
+    image_with_text = cv2.cvtColor(np.array(img_pil), cv2.COLOR_RGB2BGR)
+
+    cv2.imshow(f"{picture_number}.jpg",cv2.cvtColor(image_with_text, cv2.COLOR_BGR2RGB))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if(__name__ == "__main__"):
     main()
